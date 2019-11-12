878f46b28f066f2bd6d01f403da7d9f97eb66622
Lawrence <locaulker@gmail.com>
2019-11-12 05:56:35 -0500

    added to navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./index.css";
+import "gestalt/dist/gestalt.css";
 
 
 import App from "./components/App";
+import Navbar from "./components/Navbar";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Checkout from "./components/Checkout";
@@ -12,15 +13,17 @@ import Checkout from "./components/Checkout";
 
 import registerServiceWorker from "./registerServiceWorker";
 
-
 const Root = () => (
   <Router>
-    <Switch>
-      <Route component={App} exact path="/" />
-      <Route component={Signin} path="/signin" />
-      <Route component={Signup} path="/signup" />
-      <Route component={Checkout} path="/checkout" />
-    </Switch>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route component={App} exact path="/" />
+        <Route component={Signin} path="/signin" />
+        <Route component={Signup} path="/signup" />
+        <Route component={Checkout} path="/checkout" />
+      </Switch>
+    </React.Fragment>
   </Router>
 );
 
